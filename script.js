d02d1baa6099be85ea592a759bc52c5cf8f1cd94
jacksonloshilari7-maker <jacksonloshilari7@gmail.com>
2026-08-21 21:20:07 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-import { db, auth, storage } from "./firebase.js";
-
-console.log("TripBnA Firebase connected!");
-console.log("Auth:", auth);
-console.log("Firestore:", db);
-console.log("Storage:", storage);
